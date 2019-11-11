6cd73d93f775244c32f1215ebf4974b242a5a6f8
karl-thomas <karl.thomas925@gmail.com>
2019-11-11 10:58:55 -0600

    create new mix project with mix_test_watch

---
+++ lib/readinglist.ex
@@ -0,0 +1,18 @@
+defmodule Readinglist do
+  @moduledoc """
+  Documentation for Readinglist.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Readinglist.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
